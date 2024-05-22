027b693066bf309900b8f73acd586cb62af587f8
Jon Jackson <ocskier@gmail.com>
2024-05-22 01:24:32 -0400

    Adds basic backend express server

---
+++ server/server.js
@@ -0,0 +1,30 @@
+const chalk = require('chalk');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3001;
+
+// Serve static files from the client/dist directory
+app.use(express.static(path.join(__dirname, 'client', 'dist')));
+
+app.use((req, res, next) => {
+    console.log(chalk.green(`Request URL: ${req.url}`));
+    next();
+});
+
+// Route to return the current time
+app.get('/api/time', (req, res) => {
+    const currentTime = new Date().toLocaleTimeString();
+    res.send(`Current time is: ${currentTime}`);
+});
+
+// Route all requests to the index.html file
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
