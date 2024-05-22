0e17e545ed191e9049616718bacb0f7bb2c3cad2
Jon Jackson <ocskier@gmail.com>
2024-05-22 01:51:46 -0400

    Adds backend time to App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,29 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  // create a new state for time
+  const [time, setTime] = useState('')
+
+  // create a function to call the backend server
+  const getTime = async () => {
+    const response = await fetch('/api/time')
+    const data = await response.json()
+    setTime(data)
+  }
+
+  // call the getTime function when the component is mounted
+  useEffect(() => {
+    getTime()
+  }, [])
 
   return (
     <>
       <div>
+        <h2>{time}</h2>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const app = express();
 const port = 3001;
 
 // Serve static files from the client/dist directory
-app.use(express.static(path.join(__dirname, 'client', 'dist')));
+app.use(express.static(path.join(__dirname, '../client', 'dist')));
 
 app.use((req, res, next) => {
     console.log(chalk.green(`Request URL: ${req.url}`));
@@ -16,12 +16,12 @@ app.use((req, res, next) => {
 // Route to return the current time
 app.get('/api/time', (req, res) => {
     const currentTime = new Date().toLocaleTimeString();
-    res.send(`Current time is: ${currentTime}`);
+    res.json(`Current server time is: ${currentTime}`);
 });
 
 // Route all requests to the index.html file
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, '../client', 'dist', 'index.html'));
 });
 
 // Start the server
